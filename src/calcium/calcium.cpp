8378f6e399e9d3decbdcdca2fe9ef9fd6dfd1cad
Jack <zfccxt@gmail.com>
2023-03-18 07:29:55 +0000

    cmake skeleton

---
+++ src/calcium/calcium.cpp
@@ -0,0 +1,11 @@
+#include "calcium/calcium.hpp"
+
+#include <cstdio>
+
+namespace Calcium {
+
+void DoSomething() {
+  printf("Hello\n");
+}
+
+}
---
+++ src/calcium/calcium.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+namespace Calcium {
+
+void DoSomething();
+
+}
