746cea1e9ebd81472cb375e06047fbd9e0d4e1d8
Jack <zfccxt@gmail.com>
2023-03-21 12:51:02 +0000

    Platform and backend detection

--- src/calcium/calcium.hpp
+++ src/calcium/calcium.hpp
@@ -1,7 +1,11 @@
 #pragma once
 
-#include "calcium/opengl/opengl_instance.hpp"
-#include "calcium/opengl/opengl_window.hpp"
+#ifdef CALCIUM_BACKEND_OPENGL
+  #include "calcium/opengl/opengl_instance.hpp"
+  #include "calcium/opengl/opengl_window.hpp"
+#endif
 
-#include "calcium/vulkan/vulkan_instance.hpp"
-#include "calcium/vulkan/vulkan_window.hpp"
+#ifdef CALCIUM_BACKEND_VULKAN
+  #include "calcium/vulkan/vulkan_instance.hpp"
+  #include "calcium/vulkan/vulkan_window.hpp"
+#endif
